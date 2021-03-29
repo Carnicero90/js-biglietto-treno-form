a7bf86c48f3b63ceeca6f0026e3513e9f1592d0e
Carnicero90 <filippomontani82@gmail.com>
2021-03-29 14:58:02 +0200

    cominciata stilizzazione css, aggiunta funzione getRandInt a js/script.js per popolare .wagon e .cp-code

--- js/script.js
+++ js/script.js
@@ -2,43 +2,44 @@ var genButton = document.getElementById('gen-button');
 var delButton = document.getElementById('del-button');
 
 
-genButton.addEventListener("click", function () {
+genButton.addEventListener("click", function() {
     const priceFactor = 0.21; //price per km
     const juniorDiscount = 1.0 - 0.2;
     const seniorDiscount = 1.0 - 0.4;
 
     var userName = document.getElementById('username').value;
-    var distanceInKm = parseInt(document.getElementById('distance').value);
-
+    var distanceInKm = parseInt(document.getElementById('distance').value) || 0;
     var outputString;
     var priceCoeff;
-        switch (document.getElementById('age-range').value) {
-            case 'minorenne': 
+    switch (document.getElementById('age-range').value) {
+        case 'minorenne':
             priceCoeff = juniorDiscount;
-                outputString = "Sconto Minorenne";
-                break;
-            case 'over':
-                priceCoeff = seniorDiscount;
-                outputString = "Sconto Over 65";
-                break;
-            default:
-                priceCoeff = 1;
-                outputString = "Tariffa piena";
-        }
+            outputString = "Sconto Minorenne";
+            break;
+        case 'over':
+            priceCoeff = seniorDiscount;
+            outputString = "Sconto Over 65";
+            break;
+        default:
+            priceCoeff = 1;
+            outputString = "Tariffa piena";
+    }
 
-    var finalPrice = priceFactor*priceCoeff*distanceInKm;
+    var finalPrice = priceFactor * priceCoeff * distanceInKm;
     var el = document.getElementById('ticket-wrapper');
-    el.className = 'block';
+    el.className = '';
     populateHTML('user', userName);
     populateHTML('offer', outputString);
+    populateHTML('wagon', getRandInt(1, 10));
+    populateHTML('cp-code', getRandInt(10000, 99999));
     populateHTML('price', finalPrice.toLocaleString("it-IT", {
         minimumFractionDigits: 2,
         maximumFractionDigits: 2
     }));
- 
+
 })
 
-delButton.addEventListener("click", function () { 
+delButton.addEventListener("click", function() {
     document.getElementById('ticket-wrapper').className = 'nada';
     document.getElementById('username').value = '';
     document.getElementById('distance').value = '';
@@ -50,8 +51,8 @@ function populateHTML(id, content) {
     el.innerHTML = content;
 }
 
-
-
-
-
-
+function getRandInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
