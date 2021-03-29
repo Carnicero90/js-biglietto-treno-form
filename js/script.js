ee67f8e781ec1411b4d7cef4e91f628ffd1eb55a
Carnicero90 <filippomontani82@gmail.com>
2021-03-29 15:39:17 +0200

    aggiunta sorta di verifica dati e stilizzato ulteriormente il css

--- js/script.js
+++ js/script.js
@@ -3,39 +3,45 @@ var delButton = document.getElementById('del-button');
 
 
 genButton.addEventListener("click", function() {
+
     const priceFactor = 0.21; //price per km
     const juniorDiscount = 1.0 - 0.2;
     const seniorDiscount = 1.0 - 0.4;
 
     var userName = document.getElementById('username').value;
-    var distanceInKm = parseInt(document.getElementById('distance').value) || 0;
-    var outputString;
-    var priceCoeff;
-    switch (document.getElementById('age-range').value) {
-        case 'minorenne':
-            priceCoeff = juniorDiscount;
-            outputString = "Sconto Minorenne";
-            break;
-        case 'over':
-            priceCoeff = seniorDiscount;
-            outputString = "Sconto Over 65";
-            break;
-        default:
-            priceCoeff = 1;
-            outputString = "Tariffa piena";
-    }
+    var distanceInKm = parseInt(document.getElementById('distance').value);
 
-    var finalPrice = priceFactor * priceCoeff * distanceInKm;
-    var el = document.getElementById('ticket-wrapper');
-    el.className = '';
-    populateHTML('user', userName);
-    populateHTML('offer', outputString);
-    populateHTML('wagon', getRandInt(1, 10));
-    populateHTML('cp-code', getRandInt(10000, 99999));
-    populateHTML('price', finalPrice.toLocaleString("it-IT", {
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2
-    }));
+    if (userName === "" || !distanceInKm || distanceInKm <= 0) {
+        alert("Dati inseriti non validi")
+    } else {
+        var outputString;
+        var priceCoeff;
+        switch (document.getElementById('age-range').value) {
+            case 'under':
+                priceCoeff = juniorDiscount;
+                outputString = "Sconto Minorenne";
+                break;
+            case 'over':
+                priceCoeff = seniorDiscount;
+                outputString = "Sconto Over 65";
+                break;
+            default:
+                priceCoeff = 1;
+                outputString = "Tariffa piena";
+        }
+
+        var finalPrice = priceFactor * priceCoeff * distanceInKm;
+        var el = document.getElementById('ticket-wrapper');
+        el.className = '';
+        populateHTML('user', userName);
+        populateHTML('offer', outputString);
+        populateHTML('wagon', getRandInt(1, 10));
+        populateHTML('cp-code', getRandInt(10000, 99999));
+        populateHTML('price', finalPrice.toLocaleString("it-IT", {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
+        }));
+    }
 
 })
 
