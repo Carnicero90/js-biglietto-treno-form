69ccee909d953e4bc866e5849c43f58192413018
Carnicero90 <filippomontani82@gmail.com>
2021-03-29 20:33:03 +0200

    commentato script.js, creata qualche funzione per manipolare il contenuto dell'html e rimosso qualche div superfluo da index.html

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
+// EVENT LISTENER HTML ELEMENTS
+
 var genButton = document.getElementById('gen-button');
 var delButton = document.getElementById('del-button');
 var payButton = document.getElementById('pay-button');
 
+// EVENTS
+
+// GENERATE TICKET
 genButton.addEventListener("click", function() {
-
+    // PRICE FACTOR VALUES
     const priceFactor = 0.21; //price per km
-    const juniorDiscount = 1.0 - 0.2;
-    const seniorDiscount = 1.0 - 0.4;
-
-    var userName = document.getElementById('username').value;
+    const juniorDiscount = 1.0 - 0.2; //20% discount for under 18
+    const seniorDiscount = 1.0 - 0.4; //40% discount for over 65
     var distanceInKm = parseInt(document.getElementById('distance').value);
 
+    var userName = document.getElementById('username').value;
+
+    // VALIDATE USER DATA
     if (userName === "" || !distanceInKm || distanceInKm <= 0) {
         alert("Dati inseriti non validi")
     } else {
+        // MAIN
+
+        // SET DISCOUNT BASED ON USER SELECTED OPTION
         var outputString;
         var priceCoeff;
         switch (document.getElementById('age-range').value) {
@@ -29,9 +38,11 @@ genButton.addEventListener("click", function() {
                 priceCoeff = 1;
                 outputString = "Tariffa piena";
         }
-
+        // FINAL PRICE
         var finalPrice = priceFactor * priceCoeff * distanceInKm;
-        document.getElementById('ticket-wrapper').className = '';
+
+        // DISPLAY div#ticket-detail (CONTAINING TRAVEL DATA)
+        removeClass("ticket-detail", 'nada')
         populateHTML('user', userName);
         populateHTML('offer', outputString);
         populateHTML('wagon', getRandInt(1, 10));
@@ -44,31 +55,53 @@ genButton.addEventListener("click", function() {
 
 })
 
+// RESET PAGE CONTENT
 delButton.addEventListener("click", function() {
+    // HIDE div#ticket-detail AND UNFILL HIS .innerHTMLs
+    addClass('ticket-detail', 'nada');
     var idElementsToEmpty = ['user', 'offer', 'wagon', 'cp-code', 'price'];
     for (var element of idElementsToEmpty) {
         populateHTML(element, '');
     }
-    console.log(element)
-    document.getElementById('ticket-wrapper').className = 'nada';
+    // UNFILL div.user-form
     document.getElementById('username').value = '';
     document.getElementById('distance').value = '';
     document.getElementById('age-range').value = 'placeholder';
 })
 
-payButton.addEventListener("click", function() {
-    document.getElementById('logging-wrapper').className = '';
-    document.getElementById('main').className = 'opaq';
+payButton.addEventListener('click', function() {
+    removeClass('logging-wrapper', 'nada');
+    addClass('main', 'opaq');
     setTimeout(function() {
         location.reload();
     }, 5000);
 })
 
-function populateHTML(id, content) {
-    document.getElementById(id).innerHTML = content;
+
+// HTML MANIPULATION FUNCTIONS
+
+function populateHTML(idElement, content) {
+    // idElement --> html element's id
+    // content --> content to fill .innerHTML
+    document.getElementById(idElement).innerHTML = content;
 }
 
+function addClass(idElement, toAdd) {
+    // idElement --> html element's id
+    // toAdd --> class to add
+    document.getElementById(idElement).classList.add(toAdd);
+}
+
+function removeClass(idElement, toRemove) {
+    // idElement --> html element's id
+    // toAdd --> class to remove
+    document.getElementById(idElement).classList.remove(toRemove);
+}
+
+// MISC FUNCTIONS
+
 function getRandInt(min, max) {
+    // return a random integer between min and max (included)
     var min = Math.ceil(min);
     var max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
